14f11fd349d82f9e3e4592fc85c287d2103c6238
Aaron Hornby (Laptop) <aaron.hornby@outlook.com>
2020-02-24 19:20:58 -0700

    fixing compile errors that were happening on Linux

--- src/object-loader.h
+++ src/object-loader.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <map>
+#include <memory>
 #include <vector>
 #include <glm/glm.hpp>
 #include <string>
--- src/render-engine.h
+++ src/render-engine.h
@@ -7,6 +7,7 @@
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <memory>
 #include <vector>
 
 #include "camera.h"
